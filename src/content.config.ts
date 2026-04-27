842594b9a54b4bdbba47e8c41a67324695ad927e
macs-massimopiazza <me@massimopiazza.com>
2026-04-27 16:12:53 +0200

    feat: refactoring campi su decap cms

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,22 +1,41 @@
 import { defineCollection, z, reference } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-const works = defineCollection({
+const settings = defineCollection({
+  loader: glob({ pattern: '**/*.json', base: "./src/content/settings" }),
+  schema: z.object({
+    contact_email: z.string(),
+    github_url: z.string(),
+    linkedin_url: z.string(),
+  }),
+});
+
+const pages = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/pages" }),
+  schema: z.object({
+    title: z.string(),
+    description_sx: z.string(),
+    description_dx: z.string(),
+
+    sx_title: z.string().optional(),
+    dx_title: z.string().optional(),
+    works_title: z.string().optional(),
+    projects_title: z.string().optional(),
+    education_title: z.string().optional(),
+
+    works: z.array(reference('work_experiences')).optional(),
+    projects: z.array(reference('projects')).optional(),
+    education: z.array(reference('education')).optional(),
+  }),
+});
+
+const work_experiences = defineCollection({
   loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/works" }),
   schema: z.object({
     title: z.string(),
     company: z.string(),
     period: z.string(),
-    type: z.string(),
-  }),
-});
-
-const projects = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/projects" }),
-  schema: z.object({
-    title: z.string(),
-    status: z.string(),
-    url: z.string().optional(),
+    description: z.string(),
   }),
 });
 
@@ -25,18 +44,31 @@ const education = defineCollection({
   schema: z.object({
     title: z.string(),
     school: z.string(),
-    year: z.string(),
+    period: z.string(),
+    description: z.string(),
   }),
 });
 
-const pages = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/pages" }),
+const projects = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/projects" }),
   schema: z.object({
     title: z.string(),
-    works: z.array(reference('works')).optional(),
-    projects: z.array(reference('projects')).optional(),
-    education: z.array(reference('education')).optional(),
+    status: z.string(),
+    // Array di oggetti per i link multipli
+    links: z.array(
+      z.object({
+        label: z.string(),
+        url: z.string(),
+      })
+    ).optional(),
+    description: z.string(),
   }),
 });
 
-export const collections = { pages, works, projects, education };
+export const collections = {  
+  settings, 
+  pages, 
+  work_experiences, 
+  education, 
+  projects  
+};
